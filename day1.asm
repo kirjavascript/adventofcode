665c867b404dd5d2fecc9c339313a71fac4ebb71
snkenjoi <snkenjoi@gmail.com>
2015-12-04 23:22:21 +0000

    day in m68k ASM

---
+++ day1.asm
@@ -0,0 +1,17 @@
+    lea	($FFFF0000).w,a0
+    clr.l   d0
+
+-
+    move.b	(a0)+,d1
+    cmpi.b  #$28,d1 ; "("
+    bne.s   +
+    add.l   #1,d0
+    bra.s   -
++
+    cmpi.b  #$29,d1 ; ")"
+    bne.s   +
+    sub.l   #1,d0
+    bra.s   -
++
+
+    ; result in d0
