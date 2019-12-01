3d041ff8aa6b52d9c225505e2d0b7a7d93fb47c6
kirjavascript <snkenjoi@gmail.com>
2019-12-01 17:40:03 +0000

    some golfing

--- 2019/part2.js
+++ 2019/part2.js
@@ -1,1 +1,2 @@
-eval(fetchSync('http://ix.io/23iP').trim().split('\n').map(f=>eval(`for(t=0,f=+f;f=(0|f/3)-2,f>0;)t+=f`)).join`+`)
+eval(store.get('').split('\n').map(f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)).join`+`)
+eval(store.get('').replace(/\d+\n/g,f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)+'+'))
