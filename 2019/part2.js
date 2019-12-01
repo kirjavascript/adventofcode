6cc525b1a95c226e4bbe12187d33dd675ddb2731
kirjavascript <snkenjoi@gmail.com>
2019-12-01 19:08:53 +0000

    stuff

--- 2019/part2.js
+++ 2019/part2.js
@@ -1,2 +1,6 @@
-eval(store.get('').split('\n').map(f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)).join`+`)
-eval(store.get('').replace(/\d+\n/g,f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)+'+'))
+// browser
+eval((d=document.body.textContent.split('\n')).map(d=>(0|d/3)-2).join`+`)
+eval(d.map(f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)).join`+`)
+// nibblr
+eval(store.get``.split('\n').map(f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)).join`+`)
+eval(store.get``.replace(/\d+\n/g,f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)+'+'))
