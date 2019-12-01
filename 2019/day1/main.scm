bb5e6cd769c30b7ed94e8a38722fd88d230a7237
kirjavascript <snkenjoi@gmail.com>
2019-12-01 15:13:39 +0000

    2019 d1

---
+++ 2019/day1/main.scm
@@ -0,0 +1,29 @@
+; http://www.shido.info/lisp/scheme9_e.html
+(define (read-file file-name)
+  (let ((p (open-input-file file-name)))
+    (let loop((ls1 '()) (c (read-char p)))
+      (if (eof-object? c)
+	  (begin
+	    (close-input-port p)
+	    (list->string (reverse ls1)))
+	  (loop (cons c ls1) (read-char p))))))
+
+; str-split : Apr 2006 Doug Hoyte, hcsw.org.
+(define (str-split str ch)
+  (let ((len (string-length str)))
+    (letrec
+      ((split
+        (lambda (a b)
+          (cond
+            ((>= b len) (if (= a b) '() (cons (substring str a b) '())))
+              ((char=? ch (string-ref str b)) (if (= a b)
+                (split (+ 1 a) (+ 1 b))
+                  (cons (substring str a b) (split b b))))
+                (else (split a (+ 1 b)))))))
+                  (split 0 0))))
+
+
+(define weights (map (lambda (n) (- (floor (/ (string->number n) 3)) 2))
+      (str-split (read-file "input.txt") #\newline)))
+
+(display (apply + weights))
