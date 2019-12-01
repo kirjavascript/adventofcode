fa6e4aadf7966b772c4d17803c020a4cfa411d97
kirjavascript <snkenjoi@gmail.com>
2019-12-01 21:54:55 +0000

    more golfing

--- 2019/part2.js
+++ 2019/part2.js
@@ -3,4 +3,4 @@ eval((d=document.body.textContent.split('\n')).map(d=>(0|d/3)-2).join`+`)
 eval(d.map(f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)).join`+`)
 // nibblr
 eval(store.get``.split('\n').map(f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)).join`+`)
-eval(store.get``.replace(/\d+\n/g,f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)+'+'))
+eval(store.get``.replace(/\d+\n/g,f=>'+'+eval(`for(t=0;f=0|f/3,f>2;)t+=f-2`)))
