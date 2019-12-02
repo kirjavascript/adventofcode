43e864bb4c3865c62ecd4856c423a5d6c12ee350
kirjavascript <snkenjoi@gmail.com>
2019-12-02 14:38:08 +0000

    saved 1

--- 2019/day2/main.js
+++ 2019/day2/main.js
@@ -1,7 +1,5 @@
 input=require('fs').readFileSync('input.txt', 'utf8').trim()
-// for(d=input.split`,`,d[1]=12,d[2]=2,i=0;99!=d[i];i+=4)[o,a,b,t]=d.slice(i),d[t]=2==o?d[a]*d[b]:d[a]-+-d[b];d[0]
-// console.log(d[0])
-
-for(x=d=[];d[0]!=19690720;x++)for(d=input.split`,`,i=0,d[1]=0|x/100,d[2]=x%100;[o,a,b,t]=d.slice(i),99!=d[i];i+=4)d[t]=2==o?d[a]*d[b]:d[a]-+-d[b]
-
+for(d=input.split`,`,d[1]=12,d[2]=2,i=0;99!=d[i];i+=4)[o,a,b,t]=d.slice(i),d[t]=2==o?d[a]*d[b]:d[a]-+-d[b];d[0]
+console.log(d[0])
+for(x=d=[];d[0]!=19690720;x++)for(d=input.split`,`,i=0,d[1]=0|x/100,d[2]=x%100;[o,a,b,t]=d.slice(i),99!=d[i];i+=4)d[t]=2-o?d[a]-+-d[b]:d[a]*d[b]
 console.log(x-1);
