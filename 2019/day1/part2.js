8d545b6ebaf0c552cbc1b0a89713680583d1c101
kirjavascript <snkenjoi@gmail.com>
2019-12-02 13:14:55 +0000

    day2 part1

--- 2019/day1/part2.js
+++ 2019/day1/part2.js
@@ -1,6 +1,6 @@
 // browser
-eval((d=document.body.textContent.split('\n')).map(d=>(0|d/3)-2).join`+`)
+(d=document.body.textContent.split`\n`).reduce((t,x)=>t+=(x/3|0)-2,0)
 eval(d.map(f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)).join`+`)
 // nibblr
-eval(store.get``.split('\n').map(f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)).join`+`)
+eval(store.get``.split`\n`.map(f=>eval(`for(t=0;f=(0|f/3)-2,f>0;)t+=f`)).join`+`)
 eval(store.get``.replace(/\d+\n/g,f=>'+'+eval(`for(t=0;f=0|f/3,f>2;)t+=f-2`)))
