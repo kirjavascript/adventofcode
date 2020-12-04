a64c1e0469fe4a7aaaa7b37fe85e9e765405f649
kirjavascript <snkenjoi@gmail.com>
2020-12-04 23:16:06 +0000

    sneaky 11 chars saved

--- 2020/day3.c
+++ 2020/day3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 void main() {
-    char b[323][31];long f=1,x=0,y=0,t=0,i=0;
-    for(int ch;(ch=getchar())^EOF;ch^'\n'&&(b[i/31][i++%31]=ch));
+    char b[323][32];long f=1,x=0,y=0,t=0,i=0;
+    for(int ch;(ch=getchar())^EOF;b[i/32][i++%32]=ch);
     for(i=5;i--;f*=t)for(x=y=t=0;323>y;x=(x+((2*i)+1)%8)%31,y+=1+(i/4))
     '#'^b[y][x]||++t;printf("%lu",f);
 }
